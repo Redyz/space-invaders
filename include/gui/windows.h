cbcb3a8998d3f28e39c1aedbc7838faea83e7a14
Redyz <leredyz@gmail.com>
2014-04-29 14:03:34 -0400

    Added some logic for the SFML branch
    - Added note to the todo.txt
    - Ported some of the methods from the guincurses to the gui equivalent
    (still need to fill in the blanks)

--- include/gui/windows.h
+++ include/gui/windows.h
@@ -1,4 +1,36 @@
-#ifndef WINDOWS_H
-#define WINDOWS_H
-#define cake 5
+#ifndef WINDOW_H
+#define WINDOW_H
+#include <string>
+
+class Logic;
+class Input;
+class WINDOW;
+class Window{
+  public:
+    Window();
+    ~Window();
+    void draw();
+    void debug(std::string text);
+    void display(std::string text);
+    void display(std::string text, int x, int y);
+    void display(std::string text, int x, int y, WINDOW* window);
+    void setup(Logic *logic);
+    void clearWindow();
+    int getWidth(){ return width; }
+    int getHeight(){ return height; }
+    void initColors();
+    void inputStep();
+  private:
+    void drawScores();
+    void drawGame();
+  private:
+    //todo: SFML replace
+    //WINDOW* gameWindow;
+    //WINDOW* scoreWindow;
+    int width;
+    int height;
+    Logic* logic;
+    Input* input;
+};
+
 #endif
---
+++ src/gui/windows.cpp
@@ -0,0 +1,33 @@
+#include "gui/windows.h"
+#include "logic.h"
+
+
+Window::Window(){
+
+}
+
+Window::~Window(){
+
+}
+
+void Window::setup(Logic *logic){
+
+}
+
+void Window::clearWindow(){
+
+}
+
+void Window::inputStep(){
+
+}
+
+void Window::draw(){
+
+}
+
+void Window::debug(std::string text){
+
+}
+
+
